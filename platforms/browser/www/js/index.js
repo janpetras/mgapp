0ca5653a9d5c3be18725b327a324272f522b93c4
Ian Petras <github@persona.33mail.com>
2019-02-28 11:30:07 +0000

    inappbrowser

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -27,7 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
 
-    window.open=('http://mger.co', '_blank', 'location=no,toolbar=no');
+    var ref = cordova.InAppBrowser.open('https://mger.co', '_blank', 'location=no');
     document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
